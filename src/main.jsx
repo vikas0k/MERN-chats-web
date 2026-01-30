b6c9f493075c4b118ef8dcee08800f75ec8d8f6f
vikas kumar jhade <vikashjhade007@gmail.com>
2026-01-30 21:45:01 +0530

    Fix rendering syntax in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+ createRoot(document.getElementById('root')).render(
   
     <BrowserRouter>
       <App />
